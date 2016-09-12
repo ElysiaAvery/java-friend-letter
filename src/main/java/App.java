2453098d37890a4c4f60ac8f983fd46573bd90ef
elysia nason <elysia.avery@gmail.com>
2016-09-11 18:36:03 -0700

    add spark friend letter project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -7,14 +8,34 @@ public class App {
 
   public static void main(String[] args) {
     staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
-      return new ModelAndView(new HashMap(), "templates/hello.vtl");
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/hello.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/form", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/favorite_photos", (request, response) -> {
-      return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/favorite_photos.vtl" );
+      return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/greeting_card", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String recipient = request.queryParams("recipient");
+      String sender = request.queryParams("sender");
+      model.put("recipient", recipient);
+      model.put("sender", sender);
+      model.put("template", "templates/greeting_card.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
